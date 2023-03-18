db6bc3ab7f4568ec6754f34634ce174ea19e0290
LBissette <loganbissette@gmail.com>
2023-03-17 21:58:42 -0400

    implemented seed data

--- db/schema.sql
+++ db/schema.sql
@@ -6,19 +6,19 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
@@ -27,4 +27,4 @@ CREATE TABLE employee (
     manager_id INT,
     FOREIGN KEY (manager_id) REFERENCES employee(id)
     ON DELETE SET NULL
-);
+); 
